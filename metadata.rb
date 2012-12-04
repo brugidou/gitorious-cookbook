640c0086f1a18c9bcb3b5d51f2e5f109bafd9a46
Maxime Brugidou <m.brugidou@criteo.com>
2012-12-04 11:13:07 +0100

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Copyright 2012, "
+maintainer_email "m.brugidou@criteo.com"
+license          ""
+description      "Installs/Configures gitorious"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
---
+++ recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: gitorious
+# Recipe:: default
+#
+# Copyright 2012, Copyright 2012, Criteo
+#
