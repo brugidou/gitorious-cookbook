73db8eaf514554337c02b483fd6d940344833f04
Maxime Brugidou <m.brugidou@criteo.com>
2013-01-30 13:42:35 +0100

    Release 1.1.0

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-maintainer       "Copyright 2012, "
+maintainer       "Copyright 2012, Criteo"
 maintainer_email "m.brugidou@criteo.com"
 license          "Apache 2.0"
 description      "Installs/Configures gitorious"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.1.0"
 depends          "rabbitmq"
 depends          "passenger_apache2"
 depends          "mysql"
